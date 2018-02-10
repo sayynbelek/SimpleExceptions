9b71a5b7b7a924a2e6a75924713c63c621a1d80f
СайынБелек Алексеевич Кошкароол <sakoshkarool@ttit.local>
2018-02-10 10:16:56 +0700

    Add

---
+++ SimpleException/SimpleException/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        public const int MaxSpeed = 100;
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        private bool carIsDead;
+        private Radio theMusicBox = new Radio();
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+                CurrentSpeed += delta;
+            if (CurrentSpeed > MaxSpeed)
+            {
+                carIsDead = true;
+                CurrentSpeed = 0;
+                Exception ex = new Exception(string.Format("{0} has overheated!", PetName));
+                ex.HelpLink = "http://www.CarsRUs.com";
+                ex.Data.Add("TimeStamp", string.Format("The car exploded at {0}", DateTime.Now));
+                ex.Data.Add("Cause", "You hav a lead foot");
+                throw ex;
+            }
+            else
+                Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+        }
+    }
+}
---
+++ SimpleException/SimpleException/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Exception Example *****\n");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar.Accelerate(10);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\n*** Error! ***");
+                Console.WriteLine("Member name: {0}", e.TargetSite);
+                Console.WriteLine("Class defining member: {0}", e.TargetSite.DeclaringType);
+                Console.WriteLine("Member type: {0}", e.TargetSite.MemberType);
+                Console.WriteLine("Message: {0}", e.Message);
+                Console.WriteLine("Source: {0}", e.Source);
+                Console.WriteLine("Stack: {0}", e.StackTrace);
+                Console.WriteLine("\n-> Custom Data:");
+                if (e.Data != null)
+                {
+                    foreach (DictionaryEntry de in e.Data)
+                        Console.WriteLine("-> {0}: {1}", de.Key, de.Value);
+                }
+            }
+            Console.WriteLine("\n***** Out of exception logic *****");
+            Console.ReadLine();
+        }
+    }
+}
